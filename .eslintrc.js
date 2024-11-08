80d0ad101ff79891f83a9a7d2d163b7c10776a8f
pugazhsiva <pugazh.siva1@gmail.com>
2024-11-09 02:37:28 +0530

    First Commit,installed modules and did configs

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  module.exports = {
+    extends: ['expo', 'prettier'],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  };
+  
+};
